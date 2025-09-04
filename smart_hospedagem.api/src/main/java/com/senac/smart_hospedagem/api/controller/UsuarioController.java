165cfd1c919ea0789e77b50ca598f21bbc7c20c2
Isac <isacsouzapedroso@gmail.com>
2025-09-04 12:48:29 -0300

    Primeiro commit

---
+++ smart_hospedagem.api/src/main/java/com/senac/smart_hospedagem/api/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.senac.smart_hospedagem.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+}
---
+++ smart_hospedagem.api/src/main/java/com/senac/smart_hospedagem/api/services/TokenService.java
@@ -0,0 +1,64 @@
+package com.senac.smart_hospedagem.api.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.senac.smart_hospedagem.api.entity.Token;
+import com.senac.smart_hospedagem.api.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${spring.secretkey}")
+    private String secret;
+
+    @Value("${spring.tempo_validacao}")
+    private Long tempo_validacao;
+
+    private String emissor = "SMART_HOSPEDAGEM";
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    public String gerarToken(String usuario, String senha){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        String token = JWT.create()
+                .withIssuer(emissor)
+                .withSubject(usuario)
+                .withExpiresAt(this.gerarDataExpiracao())
+                .sign(algorithm);
+        tokenRepository.save(new Token(null, token, usuario));
+
+        return token;
+    }
+
+    public String validarToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer(emissor)
+                .build();
+        verifier.verify(token);
+        var tokenResult = tokenRepository.findByToken(token).orElse(null);
+
+        if(tokenResult == null){
+            throw new IllegalArgumentException("Token invalido");
+        }
+
+        return tokenResult.getUsuario();
+    }
+
+    public Instant gerarDataExpiracao(){
+        var dataAtual =  LocalDateTime.now();
+        dataAtual = dataAtual.plusMinutes(tempo_validacao);
+
+        return dataAtual.toInstant(ZoneOffset.of("-03:00"));
+    }
+}
